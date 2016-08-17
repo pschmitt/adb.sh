687d29ed3ee832863c1b03fdcd3e32ae0b269c84
Philipp Schmitt <philipp@schmitt.co>
2016-08-17 16:09:11 +0200

    Add plug_type function to determine what how the phone is currently connected

--- lib/battery.sh
+++ lib/battery.sh
@@ -8,3 +8,22 @@ battery_level() {
 is_charging() {
     adb shell dumpsys power | grep -q mIsPowered=true
 }
+
+plug_type() {
+    # https://developer.android.com/reference/android/os/BatteryManager.html#BATTERY_PLUGGED_AC
+    local plug_type=$(adb shell dumpsys power | sed -n 's/.*mPlugType=\([0-9]\+\).*/\1/p')
+    case "$plug_type" in
+        1)
+            echo ac
+            ;;
+        2)
+            echo usb
+            ;;
+        4)
+            echo wireless
+            ;;
+        *)
+            echo "unknown ($plug_type)"
+            ;;
+    esac
+}
