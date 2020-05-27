08d83ddc6b5adbc382f2ee15ba4440878f9291c3
Philipp Schmitt <philipp@schmitt.co>
2020-05-27 12:07:16 +0200

    Add short script for allowing join to read from the clipboard

---
+++ allow-reading-clipboard-in-background.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+usage() {
+  echo "Usage: $(basename "$0") APP_OR_PKG"
+  echo "Supported apps: Join (join)"
+  # echo "Supported apps: Termux::API (termux) and Join (join)"
+}
+
+allow() {
+  local pkg_name="$1"
+
+  # https://joaoapps.com/AutoApps/Help/Info/com.joaomgcd.join/android_10_read_logs.html
+
+  adb shell appops set "${pkg_name}" SYSTEM_ALERT_WINDOW allow
+  adb shell pm grant "${pkg_name}" android.permission.WRITE_SECURE_SETTINGS
+  adb shell pm grant "${pkg_name}" android.permission.READ_LOGS
+  adb shell am force-stop "${pkg_name}"
+}
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]
+then
+  case "$1" in
+    help|h|-h|--help)
+      usage
+      exit 0
+      ;;
+    join)
+      PKG_NAME=com.joaomgcd.join
+      ;;
+    # termux)
+    #   PKG_NAME=com.termux.api
+    #   ;;
+    *)
+      usage
+      exit 2
+      ;;
+  esac
+
+  allow "$PKG_NAME"
+fi
